17c34242bbd1d03257606976444924d9e431efdb
FlashKillerV2 <sebasmtz.flashkiller@gmail.com>
2023-12-27 11:29:53 -0600

    sparky2

--- device.mk
+++ device.mk
@@ -357,7 +357,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-spark
 
 PRODUCT_PACKAGES += \
     AospWifiResOverlayMarble \
